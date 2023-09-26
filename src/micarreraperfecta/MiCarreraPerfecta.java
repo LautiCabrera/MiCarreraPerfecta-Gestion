fa868efda629bf7af28558d851a3479148d5db0d
Ilko Esteban Arboit <ilkoarboit@gmail.com>
2023-09-25 21:43:50 -0300

    Obtención de datos y ejecución de querys
    +Actualización de JsonDataFetcher para la devolución de datos correctos
    +Actualización de DDBBConnection para ejecución de querys
    +Actualización de ResultSetIES9021
    +Agregado ejemplo de uso en Main

--- src/micarreraperfecta/MiCarreraPerfecta.java
+++ src/micarreraperfecta/MiCarreraPerfecta.java
@@ -1,61 +1,39 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package micarreraperfecta;
 
-import Utils.Conexion;
-import java.sql.*;
-import java.util.ArrayList;
+import Models.Career;
+import Utils.DDBBConnection;
+import Utils.JavaFileGenerator;
+import Utils.JsonDataFetcher;
+import Utils.ResultSetIES9021;
 
-
-/**
- *
- * @author BTF
- */
 public class MiCarreraPerfecta {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
     }
-    
-    public void Inicio(String Email) {
-        System.out.println("Email: "+Email);
-        try {
-            Conexion Conect=new Conexion();
-            Conect.Conectar();
-            ResultSet result=Conect.SendAndRecibe("select * from `ies9021_database`.users where email='"+Email.toLowerCase()+"';");
-            ArrayList<String[]> Data= new ArrayList();
-            
-            while(result.next()){
-                String[] fila=new String[3];
-                
-                fila[1]=result.getString("email");
-                fila[2]=result.getString("password");
-                if(fila[1]==null){fila[1]="";}
-                if(fila[2]==null){fila[2]="";}
-                Data.add(fila);
-            }
-            ResultSetMetaData resultdata = result.getMetaData();
-            int cantidadDColumnas= resultdata.getColumnCount();
-            for(int i=1;i<=cantidadDColumnas;i++){
-                System.out.println(resultdata.getColumnName(i)+" ");
-            }
-            int count=0;
-            for(String[] fila:Data){
-                System.out.println(fila[2]+" ("+count);
-                if(fila[1].equals(Email)){
-                    System.out.println("Found");
-                }
-                count++;
-            }
-        } catch (Exception e) {
-            System.out.println("Fail detected\n");
-            System.out.println(e.getMessage());
+
+    public void ejemplo(){
+        ResultSetIES9021<Career> carreras = new JsonDataFetcher<Career>().fetchTableData(
+            "career", "id_career < 10",Career.class);
+
+        System.out.println(carreras.getState());
+        System.out.println(carreras.getClarification());
+        System.out.println("-------------------------");
+
+        for (Career carrer : carreras.getDatos()) {
+            System.out.print("Id: " + carrer.getIdCareer());
+            System.out.print(" Nombre: " + carrer.getName());
+            System.out.print(" Descipción: " + carrer.getDescription());
+            System.out.print(" Range: " + carrer.getIdRange());
+            System.out.println("");
+            System.out.println("-------------------------");
         }
+
+        ResultSetIES9021 query = new ResultSetIES9021<>();
+        query = DDBBConnection.SendQuery("UPDATE career SET id_career = 1 WHERE id_career = 1;");
+
+        System.out.println("State: " + query.getState());
+        System.out.println("Clarification: " + query.getClarification());
+        System.out.println("-------------------------");
     }
-    
 }
