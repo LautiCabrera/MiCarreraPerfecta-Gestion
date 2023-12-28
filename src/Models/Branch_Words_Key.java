c9ed8a9a93dd67b438d9d6fae89deeefb35e185d
LautiCabrera <lau.cabrera114@gmail.com>
2023-12-28 18:13:20 -0300

    Merge

--- src/Models/Branch_Words_Key.java
+++ src/Models/Branch_Words_Key.java
@@ -1,7 +1,8 @@
 package Models;
 
-//import edu.stanford.nlp;
-
+import Utils.DDBBConnection;
+import Utils.JsonDataFetcher;
+import java.util.List;
 
 public class Branch_Words_Key {
     private int id_branch_word_key;
@@ -99,6 +100,25 @@ public class Branch_Words_Key {
     public void setF_update(String f_update) {
         this.f_update = f_update;
     }  
-
+    
+    //Métodos
+    
+    public static List<String[]> getBranchWordsKey() {
+        List<String[]> branchWordsKeyList = JsonDataFetcher.selectQuery("*", "branch_words_key", "");
+        for (String[] strings : branchWordsKeyList) {
+            System.out.println(strings);
+        }
+        return branchWordsKeyList;
+    }
+    
+    public static void deleteBranchWordsKey(int id){
+        String query = "DELETE FROM branch_words_key WHERE id_branch_word_key = "+ id;
+        DDBBConnection.SendQuery(query);
+    }
+    
+    public static void buscarPorNombreDeBranch(int id){
+        String query = "select name from branch where id_branch =" + id;
+        DDBBConnection.SendQuery(query);
+    }
 
 }
